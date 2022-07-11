e1e0584b4a6c66fa95b16cfa617fdbbaffd0a91e
itzel.du <itzel.du@kakaoenterprise.com>
2022-07-11 21:52:17 +0900

    HomeAccountCellItem에 Hashable 적용

--- Clone-Toss/Clone-Toss/HomeViewModel.swift
+++ Clone-Toss/Clone-Toss/HomeViewModel.swift
@@ -17,10 +17,19 @@ struct HomeCollectionViewHeaderItem {
   let title: String
 }
 
-struct HomeAccountCellItem {
+struct HomeAccountCellItem: Hashable {
+  let id: UUID = .init()
   let title: String
   let sumOfMoney: String
   let iconImage: CTImage?
+  
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(id)
+  }
+  
+  static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs.id == rhs.id
+  }
 }
 
 
